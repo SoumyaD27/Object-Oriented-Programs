7ba7a1a7b6ad22c892a8d180eeeeaa7c03ea091c
SoumyaD27 <soumyad233@gmail.com>
2022-12-11 22:26:31 +0530

    JSON Inventory Data Management of Rice, Pulses and Wheat.

---
+++ JsonInventory/JsonMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonInventory
+{
+    public class JsonMain
+    {
+        Model Inventory = new Model();
+        List<Model> InventoryList = new List<Model>();
+        public void convert(string jFilePath)
+        {
+            using (StreamReader reader = new StreamReader(jFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<Model>>(json);
+                Console.WriteLine("Name Weight Price TotalValue");
+                foreach(var item in items)
+                {
+                    double value = item.Price * item.Weight;
+                    Console.WriteLine(item.Name + " " + item.Weight + " " + item.Price + " " + value);
+                }
+            }
+        }
+    }
+}
---
+++ JsonInventory/Program.cs
@@ -0,0 +1,3 @@
+﻿
+JsonInventory.JsonMain json = new JsonInventory.JsonMain();
+json.convert("F:\\.NET, Visual Studio\\Object-Oriented-Programs\\JsonInventory\\Inventory.json");
